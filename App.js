9287f94d84042bdde3a7450f6ffb946ffd05bfdd
tan-duong <tanduong@live.com>
2019-02-26 22:17:50 -0600

    final commit

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import {
   View,
   ImageBackground,
   StatusBar,
-  Image
+  Image,
+  TextInput
 } from "react-native";
 
 export default class App extends React.Component {
@@ -25,32 +26,90 @@ export default class App extends React.Component {
  }}
         /> */}
         <View style={styles.commentPanel}>
+          <Image source={require("./assets/collapse.png")} style={styles.collapse}/>
           <View style={styles.commentWrapper}>
             <View style={styles.mainCommentWrapper}>
-              <Image source={require("./assets/icon-like-selected.png")}/>
+              <Image source={require("./assets/Oval.png")} style={styles.avatar}/>
               <View style={styles.commentText}>
                 <Text style={styles.mainText}>
                   jacob2 Strange man like @thedude loves to sing weird songs and
                   this girl has a nice hair color
                 </Text>
-                <Text style={styles.subText}>5min\t123 likes</Text>
+                <Text style={styles.subText}>5min  123 likes</Text>
               </View>
 
               <Image source={require("./assets/icon-like.png")}/>
             </View>
-            {/* <View style={styles.subCommentWrapper}>
-              <Image source={require("./assets/icon-like-selected.png")}/>
-              <View>
-                <Text>
+            <View style={styles.subCommentWrapper}>
+              <Image source={require("./assets/Oval-1.png")} style={styles.avatar}/>
+              <View style={styles.commentText}>
+                <Text style={styles.mainText}>
                 thedude yaaa dude
                 </Text>
-                <Text>5min\t123 likes</Text>
+                <Text style={styles.subText}>5min  123 likes</Text>
+              </View>
+
+              <Image source={require("./assets/icon-like-selected.png")}/>
+            </View>
+          </View>
+          <View style={styles.commentWrapper}>
+            <View style={styles.mainCommentWrapper}>
+              <Image source={require("./assets/Oval.png")} style={styles.avatar}/>
+              <View style={styles.commentText}>
+                <Text style={styles.mainText}>
+                  jacob2 Strange man like @thedude loves to sing weird songs and
+                  this girl has a nice hair color
+                </Text>
+                <Text style={styles.subText}>5min  123 likes</Text>
               </View>
 
               <Image source={require("./assets/icon-like.png")}/>
-            </View> */}
+            </View>
+            <View style={styles.subCommentWrapper}>
+              <Image source={require("./assets/Oval-1.png")} style={styles.avatar}/>
+              <View style={styles.commentText}>
+                <Text style={styles.mainText}>
+                thedude yaaa dude
+                </Text>
+                <Text style={styles.subText}>5min  123 likes</Text>
+              </View>
+
+              <Image source={require("./assets/icon-like.png")}/>
+            </View>
+          </View>
+          <View style={styles.commentWrapper}>
+            <View style={styles.mainCommentWrapper}>
+              <Image source={require("./assets/Oval.png")} style={styles.avatar}/>
+              <View style={styles.commentText}>
+                <Text style={styles.mainText}>
+                  jacob2 Strange man like @thedude loves to sing weird songs and
+                  this girl has a nice hair color
+                </Text>
+                <Text style={styles.subText}>5min  123 likes</Text>
+              </View>
+
+              <Image source={require("./assets/icon-like.png")}/>
+            </View>
+            
+          </View>
+          <View style={styles.bottomPanel}>
+            <View style={styles.emojiPanel}>
+            <Image source={require("./assets/emoji.png")}/>
+            <Image source={require("./assets/emoji.png")}/>
+            <Image source={require("./assets/emoji.png")}/>
+            <Image source={require("./assets/emoji.png")}/>
+            <Image source={require("./assets/emoji.png")}/>
+            <Image source={require("./assets/emoji.png")}/>
+            <Image source={require("./assets/emoji.png")}/>
+            <Image source={require("./assets/emoji.png")}/>
+            </View>
+          <View style={styles.messagePanel}>
+            <TextInput placeholder='Say something'></TextInput>
+            <Image source={require("./assets/icon-send.png")}/>
+          </View>
           </View>
         </View>
+        
       </ImageBackground>
     );
   }
@@ -68,10 +127,13 @@ const styles = StyleSheet.create({
     color: '#FFFFFF',
     fontSize: 16,
     marginLeft: 10,
-    marginBottom: 10,
+    //marginBottom: 10,
   },
   btnFollow: {
     margin: 10,
+  },
+  collapse:{
+    marginVertical: 10,
   },
   commentPanel: {
     width: "100%",
@@ -79,26 +141,70 @@ const styles = StyleSheet.create({
     position: "absolute",
     bottom: 0,
     left: 0,
-    //</View>backgroundColor: "rgba(52, 52, 52, 0.8)"
+    backgroundColor: "#FFFFFF",
+    alignItems: 'center',
+    borderRadius: 20,
+    //borderWidth: 1,
   },
   commentWrapper: {
-    width: '90%',
+    width: '100%',
+    //padding: 10,
     
+  },
+  avatar: {
+    marginLeft: 10,
+    //marginTop: 5,
   },
   mainCommentWrapper: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+    padding: 10,
   },
   commentText: {
-
+    width: '80%',
   },
   mainText: {
     fontFamily: 'Helvetica Neue',
     fontSize: 14,
+    width: '100%',
+    paddingLeft: 5,
   },
   subText: {
     fontFamily: 'Helvetica Neue',
     fontSize: 13,
-    color: '#B7B7B7'
+    color: '#B7B7B7',
+    marginTop: 10,
+    paddingLeft: 5,
+  },
+  subCommentWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
+    marginLeft: 30,
+    borderLeftWidth: 2,
+    borderLeftColor: '#F3F3F3'
+  },
+  bottomPanel: {
+    width: "100%",
+    height: 100,
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    backgroundColor: "#FFFFFF",
+    alignItems: 'center',
+    borderTopWidth: 1,
+    borderTopColor: '#C8C8C8' 
+  },
+  emojiPanel: {
+    width: "100%",
+    flexDirection:'row',
+    justifyContent: 'space-between',
+    padding: 10,
+  },
+  messagePanel: {
+    width: "100%",
+    flexDirection:'row',
+    justifyContent: 'space-between',
+    padding: 10,
   }
 });
